1619ce66a6040d9d02d6063efa90d3cd3876eae4
Arthur Clemens <arthurclemens@gmail.com>
2015-04-07 20:52:26 +0200

    use icons in buttons

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@
 // config values
 var set;
 
+var yes_icon = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" baseProfile="full" width="24" height="24" viewBox="0 0 24.00 24.00" enable-background="new 0 0 24.00 24.00" xml:space="preserve"><path fill-opacity="1" stroke-width="0.2" stroke-linejoin="round" d="M 21,7L 9,19L 3.5,13.5L 4.91421,12.0858L 9,16.1716L 19.5858,5.58579L 21,7 Z "/></svg>';
+var no_icon = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" baseProfile="full" width="24" height="24" viewBox="0 0 24.00 24.00" enable-background="new 0 0 24.00 24.00" xml:space="preserve"><path fill-opacity="1" stroke-linejoin="round" d="M 19,6.41L 17.59,5L 12,10.59L 6.41,5L 5,6.41L 10.59,12L 5,17.59L 6.41,19L 12,13.41L 17.59,19L 19,17.59L 13.41,12L 19,6.41 Z "/></svg>';
+var info_icon = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" baseProfile="full" width="24" height="24" viewBox="0 0 24.00 24.00" enable-background="new 0 0 24.00 24.00" xml:space="preserve"><path fill-opacity="1" stroke-width="0.2" stroke-linejoin="round" d="M 10.9994,8.99805L 12.9994,8.99805L 12.9994,6.99805L 10.9994,6.99805M 11.9994,19.998C 7.58838,19.998 3.99939,16.4091 3.99939,11.998C 3.99939,7.58704 7.58838,3.99805 11.9994,3.99805C 16.4104,3.99805 19.9994,7.58704 19.9994,11.998C 19.9994,16.4091 16.4104,19.998 11.9994,19.998 Z M 11.9994,1.99805C 6.47638,1.99805 1.99939,6.47504 1.99939,11.998C 1.99939,17.5211 6.47638,21.998 11.9994,21.998C 17.5224,21.998 21.9994,17.5211 21.9994,11.998C 21.9994,6.47504 17.5224,1.99805 11.9994,1.99805 Z M 10.9994,16.998L 12.9994,16.998L 12.9994,10.998L 10.9994,10.998L 10.9994,16.998 Z "/></svg>';
+
 var app = {};
 
 app.getCards = function() {
@@ -102,9 +106,9 @@ app.view = function() {
         	m('div', card[set.item_key])
         ]),
         m('.button-row[horizontal][layout]', [
-            m('a[flex]', {onclick: app.vm.next.bind(app.vm)}, 'No'),
-            m('a[flex]', {onclick: app.vm.info.bind(app.vm)}, 'Info'),
-            m('a[flex]', {onclick: app.vm.done.bind(app.vm, card)}, 'Yes')
+            m('a[flex]', {onclick: app.vm.next.bind(app.vm)}, m.trust(no_icon)),
+            m('a[flex]', {onclick: app.vm.info.bind(app.vm)}, m.trust(info_icon)),
+            m('a[flex]', {onclick: app.vm.done.bind(app.vm, card)}, m.trust(yes_icon))
         ])
     ]);
 };
