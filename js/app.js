be9cc5493a725d1451479625dfd2c3a1e9e011aa
Arthur Clemens <arthurclemens@gmail.com>
2015-04-07 20:45:05 +0200

    do not persist info status

--- js/app.js
+++ js/app.js
@@ -46,16 +46,18 @@ app.vm.init = function() {
     this.inited = m.prop(false);
     this.cardData = m.prop();
     this.cards = new app.getCards();
+    this.showInfo = m.prop(false);
 };
 app.vm.done = function(card) {
     card.done = true;
     app.storeData(app.vm.cards);
     app.vm.updateCardData();
 };
-app.vm.info = function(card) {
-	card.info = !card.info;
+app.vm.info = function() {
+	app.vm.showInfo(!app.vm.showInfo());
 };
 app.vm.next = function() {
+    app.vm.showInfo(false);
     app.vm.updateCardData();
 };
 app.vm.updateCardData = function() {
@@ -94,14 +96,14 @@ app.view = function() {
     		m('span[flex]'),
     		m('span[flex]', (cardData.all.length - cardData.remaining.length))
     	]),
-    	card.info ? m('.card-inner[horizontal][layout][center]', [
+    	app.vm.showInfo() ? m('.card-inner[horizontal][layout][center]', [
         	m('.info', m('span', card[set.item_key] + ':'), m('br'), card[set.meaning_key])
         ]) : m('.card-inner[horizontal][layout][center][center-justified]', [
         	m('div', card[set.item_key])
         ]),
         m('.button-row[horizontal][layout]', [
-            m('a[flex]', {onclick: app.vm.next.bind(app.vm, card)}, 'No'),
-            m('a[flex]', {onclick: app.vm.info.bind(app.vm, card)}, 'Info'),
+            m('a[flex]', {onclick: app.vm.next.bind(app.vm)}, 'No'),
+            m('a[flex]', {onclick: app.vm.info.bind(app.vm)}, 'Info'),
             m('a[flex]', {onclick: app.vm.done.bind(app.vm, card)}, 'Yes')
         ])
     ]);
